51bac92444e846fc89cae2d339a2430b941af78b
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-13 16:39:43 +0100

    Add js chapter 5

---
+++ js/05-arrays/main.js
@@ -0,0 +1,10 @@
+var names = ["nicolas", "alexandra", "celeste", "no name"];
+
+var tabSize = names.length;
+
+console.log(`Le nom en 2eme position dans le tableau est ${names[1]}`);
+
+console.log("Liste des valeurs de [names]");
+
+for(var i = 0, size = names.length; i < size; i++)
+    console.log(names[i]);
