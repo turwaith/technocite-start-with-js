b227fe5a845ed9a87e2b9cdbfef2dca239328962
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-13 13:34:40 +0100

    Add js chapter 2

---
+++ js/02-variables/main.js
@@ -0,0 +1,13 @@
+var str = "A string of characters";
+var integers = 102;
+var trueOrNot = true;
+var aFunction = item => console.log(item);
+var aArray = ["one", "two"];
+var obj = {name:"Nicolas", nickName:"Turwaith"};
+var undef = undefined;
+
+var str1 = "Nicolas";
+var str2 = "Javscript";
+
+console.log(`${str1} apprends le ${str2}`);
+
