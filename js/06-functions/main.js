81160ba658729b62f33fb1e19df337b81c09a20e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-14 15:59:20 +0100

    Add js chapter 6

---
+++ js/06-functions/main.js
@@ -0,0 +1,6 @@
+function saySomething(message, author){
+    console.log(`${author} said: ${message}`);
+}
+
+saySomething("Hello World !", "Nicolas");
+saySomething("Bonjour tout le monde", "Nicolas");
