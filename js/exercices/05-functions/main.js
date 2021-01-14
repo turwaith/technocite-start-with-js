8b06530a7e67dfa7953307869761e76f307e63b1
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-01-14 15:59:55 +0100

    Add js exercice 05 functions

---
+++ js/exercices/05-functions/main.js
@@ -0,0 +1,14 @@
+/**
+ *  A l'aide d'une fonction, m'effectuer le calcul comme suit:
+ *  x + y - (z*2)
+ *  La fonction retourne le résultat dans la console
+ */
+
+ function calculate(x,y,z){
+     return x + y - (z*2);
+ }
+
+ console.log(calculate(1,1,1));
+ console.log(calculate(15,17,1));
+ console.log(calculate(50,100,25));
+ console.log(calculate(33,22,22));
